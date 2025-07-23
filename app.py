e7733d9309ad7af924c1865fad1d95930846370b
hamnausman736 <f2022266736@gmail.com>
2025-07-23 23:02:35 +0500

    First Comment

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import preprocessor
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+    st.dataframe(df)
+
+    # fetch unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0, "Overall")
+
+    selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
+
+    st.sidebar.button("Show Analysis")
